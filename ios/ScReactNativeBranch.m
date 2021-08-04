b262ce9c83d5a38f9f9e75f323f0077c4e8accc7
Philipp Mayr <me@philipp-mayr.de>
2021-08-04 15:58:14 +0200

    make iOS version compatible with Android version

--- ios/ScReactNativeBranch.m
+++ ios/ScReactNativeBranch.m
@@ -7,7 +7,7 @@
 
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_REMAP_MODULE(SCBranch, SCBranchManager, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(RNBranch, SCBranchManager, NSObject)
 
 RCT_EXTERN_METHOD(initSession);
 
